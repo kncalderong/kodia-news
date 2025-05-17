0a7415967f9053686d9c72b2791c4a0048dd38e0
Kevin Nicolas <kncalderong@gmail.com>
2025-05-17 14:49:32 -0500

    feat: feed page modified to contain search and articles grid, loaded by suspensee block to handle async stream

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,22 @@
-import { Metadata } from "next";
-import Image from "next/image";
-import { defineQuery } from "next-sanity";
+import Search from "@/components/feed/Search";
+import { Suspense } from "react";
+import ArticlesGrid from "@/components/feed/ArticlesGrid";
 
-import { client } from "@/sanity/client";
-import { HomepageQueryResult } from "@/sanity/types";
-
-import logoKodia from "@/assets/images/logo-kodia.png";
-
-const HomepageQuery = defineQuery(`*[_type == "article"]`);
-
-export const metadata: Metadata = {
-  title: "Kodia News",
-  description: "Kodia News",
-};
-
-export default async function Home() {
-  const _ = await client.fetch<HomepageQueryResult>(HomepageQuery);
+export default async function Home(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
 
   return (
-    <main className="flex justify-center items-center bg-white w-screen h-screen">
-      <div className="z-10 space-y-4 w-full max-w-3xl">
-        <h1 className="font-bold text-black text-4xl">Kodia News</h1>
-        <p className="text-black text-lg">Under construction.</p>
-        <hr className="opacity-50 border-black/25" />
-        <Image
-          src={logoKodia}
-          alt="Kodia Logo"
-          className="w-auto h-5 object-contain"
-        />
+    <main className="container flex justify-center p-4  md:p-6 lg:p-8">
+      <div className="flex flex-col gap-4 w-full md:gap-6 lg:gap-8">
+        <Search placeholder="Search by name or author" />
+        <Suspense key={query} fallback={"Loading..."}>
+          <ArticlesGrid query={query} />
+        </Suspense>
       </div>
     </main>
   );
