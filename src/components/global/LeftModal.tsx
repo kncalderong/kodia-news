3640f9896b097ecea97e1d6bfb0b8977d47ce7b2
Kevin Nicolas <kncalderong@gmail.com>
2025-05-17 21:24:43 -0500

    fix: mobile sidebar bg classname typo

--- src/components/global/LeftModal.tsx
+++ src/components/global/LeftModal.tsx
@@ -35,7 +35,7 @@ const LeftModal: React.FC<LeftModalProps> = ({
           isOpen ? "translate-x-0" : "-translate-x-[101%]"
         )}
       >
-        <div className="flex flex-col h-full gap-6 px-4 py-5 bg-semi-dark-blueborder-r-gray-200 md:py-8 md:px-6 ">
+        <div className="flex flex-col h-full gap-6 px-4 py-5 bg-semi-dark-blue border-r-gray-200 md:py-8 md:px-6 ">
           <div
             className={classNames(
               "flex items-center w-full",
